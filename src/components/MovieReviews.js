de95b972f50add9f6c9a1a87c3b4cf8e5293e704
Zach Frohardt <zfrohardt@gmail.com>
2021-04-29 11:56:13 -0700

    completed assignment

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,17 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = props => (
+    <div className='review-list'>
+        {props.reviews.map(displayReview)}
+    </div>
+);
+
+const displayReview = (review, index) => (
+    <div key={index} className='review'>
+        <h1>{review.display_title}</h1>
+        <h2>{review.byline}</h2>
+    </div>
+)
+
+export default MovieReviews;
